054bc374db46f3b5c10c8e1869c60e0a450dde61
JDgomez <87883400+JDgomez2002@users.noreply.github.com>
2023-05-08 11:04:43 -0600

    User Event maps and Login title

--- src/features/render/RenderEvents/UserEvents.jsx
+++ src/features/render/RenderEvents/UserEvents.jsx
@@ -1,30 +1,88 @@
 import * as styles from './UserEvents.module.css'
 import Event from '@components/global/Event'
 import useRefreshToken from '@hooks/useRefreshToken'
+import { useState, useEffect } from 'react'
+
+const events = [
+  {
+    title: "Call of duty",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "League of Legends",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "Minecraft",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "Chess",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "FIFA",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "Need for Speed",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "Cuphead",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "Spiderman PS4",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+  {
+    title: "NBA 2k24",
+    hour: "11:23",
+    people: "Pablo, Andres",
+    link: "https://discord.gg/XvNTrMyE",
+  },
+]
 
 function UserEvents() {
   const refresh = useRefreshToken()
+  const [userEvents, setUserEvents] = useState([])
+
+  useEffect(() => {
+    setUserEvents(events)
+  }, [])
 
   return (
     <div className={styles.root}>
-      <h1>Events</h1>
+      <h1>My Events</h1>
       <button onClick={() => refresh()}>Refresh</button>
       <div className={styles.container}>
-        <Event
-          name="Call of duty"
-          hour="11:23"
-          people="Pablo, Andres, Samuel"
-        />
-        <Event name="League of Legends" />
-        <Event name="Minecraft" />
-        <Event name="Chess" />
-        <Event name="FIFA" />
-        <Event name="Need for Speed" />
-        <Event name="Cuphead" />
-        <Event name="Spiderman PS4" />
-        {/* <Event 
-          name="Spiderman PS5"
-        /> */}
+        {
+          userEvents.map( event => (
+            <Event
+              name = {event.title}
+              hour = {event.hour}
+              people = {event.people}
+              link = {event.link}
+            />
+          ))
+        }
       </div>
     </div>
   )
