568b10fb17766e068ca6096465771b19e97a70d6
chamale-rac <samuel.chamale23@gmail.com>
2023-05-10 23:40:34 -0600

    Username name in the welcome page

--- src/features/render/HomePage/HomePage.jsx
+++ src/features/render/HomePage/HomePage.jsx
@@ -1,10 +1,16 @@
 import * as styles from './HomePage.module.css'
+import { authStore } from '@context'
 
 function HomePage() {
+  const { auth } = authStore
+
   return (
-    <div className={styles.container} >
-      <h1>Welcome back 'UserName'!</h1>
-      <h2>At Overcome we want you to have fun while you practice your social skills!</h2>
+    <div className={styles.container}>
+      <h1>Welcome back {auth.user.username}!</h1>
+      <h2>
+        At Overcome we want you to have fun while you practice your social
+        skills!
+      </h2>
     </div>
   )
 }
