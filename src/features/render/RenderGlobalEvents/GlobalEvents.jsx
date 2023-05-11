ed0cf40f2c2016db67d60c97dfe8642fe9eaa303
JDgomez <87883400+JDgomez2002@users.noreply.github.com>
2023-05-11 01:28:45 -0600

    Search Events

--- src/features/render/RenderGlobalEvents/GlobalEvents.jsx
+++ src/features/render/RenderGlobalEvents/GlobalEvents.jsx
@@ -3,12 +3,27 @@ import { useApi } from '@hooks'
 import * as styles from './GlobalEvents.module.css'
 import Event from '@components/global/Event'
 
+function findSimilarStrings(inputString, stringArray) {
+  const similarStrings = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    const currentString = stringArray[i].title;
+    if (currentString.includes(inputString)) {
+      similarStrings.push(stringArray[i]);
+    }
+  }
+  return similarStrings;
+}
+
+
 function GlobalEvents() {
   const [userEvents, setUserEvents] = useState([])
   const { handleRequest } = useApi()
+  const [searchInput, setSearchInput] = useState("")
+  const [searching, setSearching] = useState(false)
 
   useEffect(() => {
     getEvents()
+    setSearching(false)
   }, [])
 
   const getEvents = async () => {
@@ -17,12 +32,39 @@ function GlobalEvents() {
     setUserEvents(response.data)
   }
 
+  const SearchBar = () => {
+    return (
+      <div className={styles.searchBarContainer} > 
+        <div className={styles.searchBarOutline}>
+          <input
+            type="text"
+            // id="header-search"
+            placeholder="Search Events"
+            name="s"
+            // onChange={setSearching(false)}
+            // onClick={setSearching(false)}
+          />
+          <button 
+            type="submit"
+            onChange={setSearching(false)}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Community Events!</h1>
+      <div className={styles.flexContainer} >
+        <h1>Community Events!</h1>
+        <SearchBar />
+      </div>
       <h2>This are today's Hooks!</h2>
       <div className={styles.eventsContainer}>
-        {userEvents.map((event) => (
+        {!searching &&
+        userEvents.map((event) => (
           <Event
             name={event.title}
             hour={event.hour}
@@ -30,6 +72,7 @@ function GlobalEvents() {
             people={event.participants}
             link={
               event.tags != undefined &&
+              "Tags: "+
               JSON.stringify(event.tags)
                 .replace('[', '')
                 .replace(']', '')
@@ -37,7 +80,8 @@ function GlobalEvents() {
                 .replace(/,/g, ', ')
             }
           />
-        ))}
+        ))
+        }
       </div>
     </div>
   )
