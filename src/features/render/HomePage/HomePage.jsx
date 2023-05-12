43c46b0a9ced5b3352f799a12d976b6b4e4f8155
Adrian R <adrian400n@gmail.com>
2023-05-12 15:44:20 -0600

    Home screen ideas

--- src/features/render/HomePage/HomePage.jsx
+++ src/features/render/HomePage/HomePage.jsx
@@ -1,13 +1,39 @@
 import * as styles from './HomePage.module.css'
+import { authStore } from '@context'
 
 function HomePage() {
+  const { auth } = authStore
+
   return (
     <div className={styles.container}>
-      <h1>Welcome back 'UserName'!</h1>
+      <h1>Welcome back {auth.user.username}</h1>
       <h2>
-        At Overcome we want you to have fun while you practice your social
-        skills!
+        What people are you meeting today? <br />
       </h2>
+      <div className={styles.board}>
+        <div className={styles.boardItem}>
+          <h3>🎉 Recent events:</h3>
+          <li>
+            <ul>Event 1</ul>
+            <ul>Event 2</ul>
+            <ul>Event 3</ul>
+          </li>
+        </div>
+        <div className={styles.boardItem}>
+          <h3>📌 Pinned Games:</h3>
+          <li>
+            <ul>Game 1</ul>
+            <ul>Game 2</ul>
+            <ul>Game 3</ul>
+          </li>
+        </div>
+        <div className={styles.boardItem}>
+          <h3>🗨️ Recent Chats:</h3>
+        </div>
+        <div className={styles.boardItem}>
+          <h1>+</h1>
+        </div>
+      </div>
     </div>
   )
 }
