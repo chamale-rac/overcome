e4445eb957995092dccc23c9ffdcbcd50eff2b15
chamale-rac <samuel.chamale23@gmail.com>
2023-05-12 00:39:29 -0600

    Finished

--- src/components/global/ClockLoader/ClockLoader.jsx
+++ src/components/global/ClockLoader/ClockLoader.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './ClockLoader.module.css'
 
-const ClockLoader = ({ fontSize = '1.2rem' }) => {
+const ClockLoader = ({ fontSize = '1.2' }) => {
   return (
     <div
       className={styles.loader}
--- src/components/global/Event/Event.jsx
+++ src/components/global/Event/Event.jsx
@@ -1,15 +1,14 @@
 import * as styles from './Event.module.css'
 
 function Event({ name, people, hour, date, link }) {
-
   return (
-      <div className={styles.container} >
-        <h1>{name}</h1>
-        <p>Hour: {hour}</p>
-        <p>Date: {date}</p>
-          <div>People: {people}</div>
-        <a>{link}</a>
-      </div>
+    <div className={styles.container}>
+      <h1>{name}</h1>
+      <p>Hour: {hour}</p>
+      <p>Date: {date}</p>
+      <div>People: {people.map((person) => person.username).join(', ')}</div>
+      <a>{link}</a>
+    </div>
   )
 }
 
