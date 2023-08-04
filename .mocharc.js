937ef51568e7c98ec549adc110358fe4d361ae80
JDgomez2002 <gom21429@uvg.edu.gt>
2023-08-04 10:11:43 -0600

    Mocha environment

---
+++ .mocharc.js
@@ -0,0 +1,17 @@
+// export const recursive = true;
+// export const slow = 15;
+// export const timeout = 5000;
+// export const spec = [
+//   'src/**/*.test.js',
+//   'src/**/*.spec.js',
+// ];
+
+module.exports = {
+  recursive: true,
+  slow: 15,
+  timeout: 5000,
+  spec: [
+    'src/**/*.test.js',
+    'src/**/*.spec.js',
+  ],
+};
