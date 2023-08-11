76f52866be504b17d38425d82324df4ba8b569dc
chamale-rac <samuel.chamale23@gmail.com>
2023-08-11 08:22:01 -0600

    config and feat: vitest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { expect, afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import matchers from '@testing-library/jest-dom/matchers'
