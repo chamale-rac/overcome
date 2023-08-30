f38ee356288884daf95ac6d95418eef158d8c19f
chamale-rac <samuel.chamale23@gmail.com>
2023-08-29 21:39:19 -0600

    feat: specific file type

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import App from '@routes/App'
+import App from '@routes/App.jsx'
 import '@assets/global.css'
 
 // TODO check for correct root (not anymore, I think)
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<App />}/>
+        <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
