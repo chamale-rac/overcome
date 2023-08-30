dd7720f4e172ac6c122a43883abfe61c075d1334
chamale-rac <samuel.chamale23@gmail.com>
2023-08-29 20:53:10 -0600

    feat: a

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // docs:: https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 4444,
   },
   plugins: [react()],
