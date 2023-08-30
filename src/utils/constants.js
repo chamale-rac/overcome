4c2e377752cd67c08bd9888dfad962fe69526a6e
chamale-rac <samuel.chamale23@gmail.com>
2023-08-29 20:24:37 -0600

    feat: server base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const SERVER_BASE_URL = 'http://127.0.0.1:3000'
+export const SERVER_BASE_URL = 'https://overcome-api.onrender.com'
 export const SERVICES_BASE_URL = 'http://127.0.0.1:8080'
 
 export const PROFILE_IMAGES_HEIGHT = 256
