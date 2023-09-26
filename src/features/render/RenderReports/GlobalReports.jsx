5055d5e62e002475b40b1bcf9f7f72b50c15414c
JosueS22 <josuesagas22@outlook.com>
2023-09-26 08:27:01 -0600

    reports list

---
+++ src/features/render/RenderReports/GlobalReports.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import { useApi } from '@hooks'
+import * as styles from './GlobalReports.module.css'
+import Reports from '../Reports/Reports'
+import { ClockLoader } from '@components/global'
+
+function GlobalReports() {
+
+  const [reports, setReports] = useState([]);
+  const { handleRequest } = useApi()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    getAllReports();
+  }, []);
+
+  const getAllReports = async () => {
+    setLoading( true );
+    try {
+        const response = await handleRequest('GET', '/reports?type=Event', {}, {}, false);
+        setReports( response.data?.data?.reports );
+        setLoading( false );
+    } catch (error) {
+        setLoading( false );
+    };
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.flexContainer}>
+        <h1>Reports</h1>
+      </div>
+
+
+      {reports.length > 0 ? (
+        <div className={styles.eventsContainer}>
+          <Reports reports={reports}/>
+        </div>
+      ) : loading ? (
+        <div className={`${styles.noEvents} font-bebas-neue`}>
+          Loading... <ClockLoader fontSize={'3.8'} />
+        </div>
+      ) : (
+        <div className={`${styles.noEvents} font-bebas-neue`}>
+          No reports found! 😔
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default GlobalReports
---
+++ src/features/render/RenderReports/index.js
@@ -0,0 +1,2 @@
+import GlobalReports from "./GlobalReports";
+export default GlobalReports
