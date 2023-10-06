15ad0248c01ce176c5f3b4327214ab4c500c7e44
chamale-rac <samuel.chamale23@gmail.com>
2023-10-05 23:04:49 -0600

    feat: CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,58 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb-base', 'airbnb/rules/react', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
-  overrides: [[{
-    files: ['*.js', '*.jsx'],
-    rules: {
-      'react/function-component-definition': 'off'
-    }
-  }]],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-base',
+    'airbnb/rules/react',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        'react/function-component-definition': 'off',
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     semi: ['error', 'never'],
-    'max-len': ['warn', {
-      code: 120
-    }],
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+      },
+    ],
     'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: true,
-      optionalDependencies: true,
-      peerDependencies: true,
-      bundledDependencies: true
-    }],
-    'react/function-component-definition': ['error', {
-      namedComponents: 'arrow-function',
-      unnamedComponents: 'arrow-function'
-    }]
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: true,
+        peerDependencies: true,
+        bundledDependencies: true,
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
   // TODO remove this temporal fix for import/no-extraneous-dependencies error on main.jsx
   settings: {
-    'import/core-modules': ['react-router-dom']
-  }
-};
+    'import/core-modules': ['react-router-dom'],
+  },
+}
