be119fec1ec8ad17c3808f093f410454d1fc6931
JDgomez2002 <gom21429@uvg.edu.gt>
2023-11-17 14:05:53 -0600

    redirect to the event and upcoming events

--- src/components/global/EventPreview/EventPreview.jsx
+++ src/components/global/EventPreview/EventPreview.jsx
@@ -1,19 +1,24 @@
 import * as styles from './EventPreview.module.css'
+import { useNavigate } from 'react-router-dom'
 
 function EventPreview({
   events,
 }) {
 
+  const navigate = useNavigate()
   return (
   <>
     {!(events === undefined) && (
       events.map((event) => (
-        <aside className={styles.border} >
-        {/* title */}
-        <p id={styles['previewTitle']} >{event.title}</p>
-        <p>-&gt; {event.date.substring(0,10)}</p>
-        <p>-&gt; {event.hour}</p>
-      </aside>
+        <aside
+          className={styles.border}
+          onClick={() => navigate(`/home/events/${event._id}`)}
+        >
+          {/* title */}
+          <p id={styles['previewTitle']} >{event.title}</p>
+          <p>-&gt; {event.date.substring(0,10)}</p>
+          <p>-&gt; {event.hour}</p>
+        </aside>
       )).slice(-3).reverse()
     )}
   </>
